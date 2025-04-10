610eef437ebf41720a350d73c4ac992afff414bd
ericlewisplease <ericl@humane.com>
2025-04-10 05:06:29 -0400

    meh

--- train.py
+++ train.py
@@ -34,9 +34,9 @@ from export import model_export
 # -----------------------------------------------------------------------------
 # I/O
 out_dir = "out"
-eval_interval = 2000
+eval_interval = 1000
 log_interval = 1000
-eval_iters = 100
+eval_iters = 40
 eval_only = False  # if True, script exits right after the first eval
 always_save_checkpoint = False  # if True, always save a checkpoint after each eval
 init_from = "scratch"  # 'scratch' or 'resume'
