388eb2860db581b403fe601c236e25ce944d94a9
ericlewisplease <ericl@humane.com>
2025-04-10 14:29:21 -0400

    sdf

--- train.py
+++ train.py
@@ -142,7 +142,7 @@ iter_batches = partial(
     vocab_size=vocab_size,
     vocab_source=vocab_source,
     device=device,
-    num_workers=30,
+    num_workers=63,
 )
 
 # init these up here, can override if init_from='resume' (i.e. from a checkpoint)
